f257cea7bb8f1c0e3d3ec98643f408b488102ed8
azat-312 <azat.myrzabekov05@gmail.com>
2025-01-09 20:45:56 +0600

    work2

---
+++ handlers/echo.py
@@ -0,0 +1,11 @@
+from aiogram import Dispatcher , types
+
+#dp.message_handler()
+async def echo_handler(message: types.Message):
+    if message.text.isdigit():
+        await message.answer(int(message.text)**2)
+    else:
+        await message.answer(message.text)
+
+def register_handlers(dp: Dispatcher):
+    dp.register_message_handler(echo_handler)
