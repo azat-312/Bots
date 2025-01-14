651e2ee951829f9941d69d90b1766c733ba08105
azat-312 <azat.myrzabekov05@gmail.com>
2025-01-14 21:19:32 +0600

    work3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from aiogram import executor
 import logging
 from config import bot, Admins, dp
-from handlers import commands, echo, quiz, webapp
+from handlers import commands, echo, quiz, webapp, FSM_registration
+from buttons import start
 
 async def on_startup(_):
     for admin in Admins:
@@ -11,6 +12,7 @@ async def on_startup(_):
 commands.register_handlers(dp)
 quiz.register_handlers(dp)
 webapp.register_handlers(dp)
+FSM_registration.register_handlers_fsm_reg(dp)
 
 echo.register_handlers(dp)
 
